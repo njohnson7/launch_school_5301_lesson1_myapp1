3b8fd1207000f40cb6b7fc1b6699baf03fe9a78f
Nick Johnson <njohnson7@users.noreply.github.com>
2018-06-18 15:51:59 -0700

    finished myapp1

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_16_023812) do
+ActiveRecord::Schema.define(version: 2018_06_18_211236) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
